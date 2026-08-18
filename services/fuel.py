94abaec8ef48f9e4799a243f59945adea4583381
kostrikin552-hub <kostrikin552@gmail.com>
2026-08-18 18:57:31 +0300

    Update fuel.py

--- services/fuel.py
+++ services/fuel.py
@@ -1,1 +1,6 @@
+import logging
+logger = logging.getLogger(__name__)
 
+async def refresh_prices():
+    """В будущем здесь будет логика сбора цен из внешних источников."""
+    logger.info("Обновление цен (заглушка)")
