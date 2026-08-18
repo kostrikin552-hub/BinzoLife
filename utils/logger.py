82b866a83ddd26e9f07f0113f741e10a2f671943
kostrikin552-hub <kostrikin552@gmail.com>
2026-08-18 19:00:20 +0300

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,1 +1,10 @@
+import logging
+from config import settings
 
+logging.basicConfig(
+    level=settings.LOG_LEVEL,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+
+def get_logger(name: str) -> logging.Logger:
+    return logging.getLogger(name)
