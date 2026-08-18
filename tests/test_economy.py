3838b73a2be6334f11298b901ea878bb2e8aedea
kostrikin552-hub <kostrikin552@gmail.com>
2026-08-18 19:01:15 +0300

    Update test_economy.py

--- tests/test_economy.py
+++ tests/test_economy.py
@@ -1,1 +1,7 @@
+from services.economy import calculate_potential_saving
 
+def test_economy():
+    saving = calculate_potential_saving(65.0, 67.0, 50)
+    assert saving == 100.0
+    saving_zero = calculate_potential_saving(67.0, 65.0, 50)
+    assert saving_zero == 0.0
