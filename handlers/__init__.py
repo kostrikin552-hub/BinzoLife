2f1d3d29a709a11386c642333d8b3e12b1870dcd
kostrikin552-hub <kostrikin552@gmail.com>
2026-09-15 13:14:37 +0300

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,7 +1,6 @@
 # handlers/__init__.py — УБРАН admin
 from . import start
 from . import find
-from . import emergency
 from . import menu
 from . import payments
 from . import profile
@@ -10,4 +9,3 @@ from . import contest
 from . import notifications
 from . import inline
 from . import common
-# admin убран — будем импортировать отдельно в main.py
